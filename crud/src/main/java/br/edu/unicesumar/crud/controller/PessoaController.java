3d9942a9b01b0158b6c9c7451ddd28963def04d6
Yvesmorello <124888523+Yvesmorello@users.noreply.github.com>
2023-10-17 20:19:17 -0300

    Add files via upload

--- crud/src/main/java/br/edu/unicesumar/crud/controller/PessoaController.java
+++ crud/src/main/java/br/edu/unicesumar/crud/controller/PessoaController.java
@@ -1,60 +1,46 @@
 package br.edu.unicesumar.crud.controller;
 
+
 import br.edu.unicesumar.crud.model.Pessoa;
-import br.edu.unicesumar.crud.service.PessoaService;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Arrays;
 import java.util.List;
-import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/pessoa")
 public class PessoaController {
 
-    private final PessoaService service;
+    //get /id ->
+    //put /id ->
 
+    @PutMapping("/{id}")
+    public Pessoa update(@PathVariable Long id, @RequestBody Pessoa pessoaUpdate){
+       Pessoa pessoEdit = mock().stream().filter(pessoa->pessoa.getId().equals(id))
+               .findFirst().orElse(null);
 
-    public PessoaController(PessoaService service) {
-        this.service = service;
+       if(pessoEdit == null) {
+           return null;
+       }
+       return new Pessoa(pessoEdit.getId(), pessoaUpdate.getNome(), pessoaUpdate.getDocumento());
     }
 
-    @GetMapping
-    public List<Pessoa> all(){
-        return service.selectAll();
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable Long id){
+        Pessoa pessoaRemove = mock().stream().filter(pessoa->pessoa.getId().equals(id))
+                .findFirst().orElse(null);
+
+        mock().remove(pessoaRemove);
     }
-
-
-    @GetMapping("/{id}")
-    public Pessoa getId(@PathVariable Long id){
-        return service.selectAll().stream().filter(p -> p.getId().equals(id)).findFirst().orElse(null);
-
-    }
-
-    @GetMapping("/pesquisa"){
-        public List<Pessoa> getByNome(@RequestParam("valor" String valor){
-            CharSequence valor;
-            return  mock().stream().filter(pessoa -> pessoa.getNome().contains(valor)).collect(Collectors.toList());
-        })
-    }
-
-
-    @PostMapping
-    public Pessoa create(@RequestBody Pessoa pessoa){
-     long maxId = mock().stream().mapToLong(Pessoa::getId).max().orElse(0L);
-
-     return new Pessoa(maxId+1L, pessoa.getNome(), pessoa.getDocumento());
-
-    }
-
-
 
     private List<Pessoa> mock() {
-        return Arrays.asList(new Pessoa(1L, "Antonio","123"),
-                            (new Pessoa(2L, "Bento","456"),
-                            (new Pessoa(3L, "João","789"),
-                            (new Pessoa(4L, "Joaquim","189"),
-                            (new Pessoa(5L, "Monica","999"),
-                            (new Pessoa(6L, "Magali","888")
+        return Arrays.asList(new Pessoa(1L, "Maria","123"),
+                             new Pessoa(2L, "Magali","321"),
+                             new Pessoa(3L, "Ronaldo","987"),
+                             new Pessoa(4L, "João","456"),
+                             new Pessoa(5L, "Joaquim","654"),
+                             new Pessoa(6L, "Roberto","789"),
+                             new Pessoa(7L, "Eduardo","741"),
+                             new Pessoa(8L, "Alvaro","852"));
     }
 }
